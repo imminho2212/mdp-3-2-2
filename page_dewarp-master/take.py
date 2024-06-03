1bfea63b4968c3aad855766bb7b51ab985b39df9
imminho2212 <130457497+imminho2212@users.noreply.github.com>
2024-06-03 15:52:48 +0900

    Update take.py

--- page_dewarp-master/take.py
+++ page_dewarp-master/take.py
@@ -1,35 +1,43 @@
 import cv2
+import serial
 import os
-# import multiprocessing
 
-# def pagedewarpAndTopdf():
-    # os.system("python C:/Users/user/Downloads/mdp-3-1-2-main/page_dewarp-master/page_dewarp.py")
-    # os.system("python C:/Users/user/Downloads/mdp-3-1-2-main/page_dewarp-master/img_to_pdf.py")
 
+port = '/dev/ttyAMA0'
+baud = 115200
 count = 1
+
+ser = serial.Serial(port, baud, timeout=0.5)
 
 cap = cv2.VideoCapture(0)     
 
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 640)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1020)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
 if cap.isOpened():                    
     while True: 
+        received_data = ser.read()
+        data_left = ser.inWaiting()           
+        received_data += ser.read(data_left)
+        print(received_data)
         ret, img = cap.read()           
         if ret:                        
-            showimg = cv2.line(img,(640,0),(640,640),(0,0,255))
-            leftimage = img[0:640,0:639].copy()
-            rightimage = img[0:640,641:1280].copy()
-            cv2.imshow('a', showimg)
-            rightimage = cv2.resize(rightimage,dsize=(0,0),fx=2,fy=2) 
-            leftimage = cv2.resize(leftimage,dsize=(0,0),fx=2,fy=2) 
+            showimg = cv2.line(img,(510,0),(510,720),(0,0,255))
+            leftimage = img[0:720,0:509].copy()
+            rightimage = img[0:720,511:1020].copy()
+            cv2.imshow('', showimg) 
             key = cv2.waitKey(25)           
             if key == 27:
                 break
             if key == 13:
-                cv2.imwrite('C:/Users/user/Downloads/mdp-3-1-2-main/page_dewarp-master/input/image{0}.jpg'.format(count), leftimage )
-                cv2.imwrite('C:/Users/user/Downloads/mdp-3-1-2-main/page_dewarp-master/input/image{0}.jpg'.format(count+1),rightimage)
+                cv2.imwrite('/home/pi/mdp/page_dewarp-master/input/image{0}.jpg'.format(count), leftimage )
+                cv2.imwrite('/home/pi/mdp/page_dewarp-master/input/image{0}.jpg'.format(count+1),rightimage)
                 count += 2
+            if received_data == b'start':
+                cv2.imwrite('/home/pi/mdp/page_dewarp-master/input/image{0}.jpg'.format(count), leftimage )
+                cv2.imwrite('/home/pi/mdp/page_dewarp-master/input/image{0}.jpg'.format(count+1),rightimage)
+                count += 2
+                ser.write(b'next\n')
         else:                          
             break
         
@@ -37,4 +45,3 @@ else:
     print("Can't open video.")         
 cap.release()                         
 cv2.destroyAllWindows()
-
