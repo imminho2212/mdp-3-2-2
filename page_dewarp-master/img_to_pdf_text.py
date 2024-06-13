7e0f61f454d0d8ff093c0732d49457c13cbd8118
imminho2212 <130457497+imminho2212@users.noreply.github.com>
2024-06-13 19:15:17 +0900

    Update img_to_pdf_text.py

--- page_dewarp-master/img_to_pdf_text.py
+++ page_dewarp-master/img_to_pdf_text.py
@@ -7,35 +7,33 @@ from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
 import os
 import easyocr
-import numpy as np
 import cv2
-import random
 import matplotlib.pyplot as plt
 import PIL
 from PIL import ImageFont, ImageDraw, Image
 
-pdfmetrics.registerFont(TTFont("나눔고딕", "C:/Users/user/Downloads/mdp-3-1-2-main/page_dewarp-master/NanumGothic-Bold.ttf"))
+pdfmetrics.registerFont(TTFont("나눔고딕", "/home/pi/mdp/page_dewarp-master/NanumGothic-Bold.ttf"))
 
 # PDF 파일 경로와 추가할 이미지의 경로를 지정합니다.
-pdf_path = "C:/Users/user/Downloads/mdp-3-1-2-main/page_dewarp-master/output.pdf"
-image_path = "C:/Users/user/Downloads/mdp-3-1-2-main/page_dewarp-master/output"
-np.random.seed(42)
-COLORS = np.random.randint(0, 255, size=(255, 3),dtype="uint8")
-reader = easyocr.Reader(['ko', 'en'], gpu = True)
+pdf_path = "/home/pi/mdp/page_dewarp-master/output_ocr.pdf"
+image_path = "/home/pi/mdp/page_dewarp-master/output"
+reader = easyocr.Reader(['ko', 'en'], gpu = False)
 
 def add_image_to_pdf(pdf_path, image_path):
-    c = canvas.Canvas(pdf_path, pagesize=letter)
-    width, height = letter
+    c = canvas.Canvas(pdf_path, pagesize=(900,1275))
+    width, height = (900,1275)
     file_list = os.listdir(image_path)
-    file_path = os.path.abspath(__file__).replace('img_to_pdf_text.py','output').replace('\\','/')
+    file_path = os.path.abspath(__file__).replace('image_to_pdf_text.py','output').replace('\\','/')
     print(file_list,file_path)
     
     for page_number in range(0, len(file_list)):
         currentimgname = file_path + '/' + file_list[page_number]
-        savename = file_path + '/qwe' + file_list[page_number]
+        savename = file_path.replace('output','qwe') + '/' + file_list[page_number]
         img = cv2.imread(currentimgname)
+        img = cv2.resize(img,(900,1275))
+        cv2.imwrite(savename,img)
         result = reader.readtext(img)
-        pil_img = Image.open(currentimgname)
+        pil_img = Image.open(savename)
         draw = ImageDraw.Draw(pil_img)
         for i in result:
             draw.rectangle([(i[0][0][0],i[0][0][1]),(i[0][1][0],i[0][2][1])],fill="#ffffff",width=3)
@@ -50,7 +48,7 @@ def add_image_to_pdf(pdf_path, image_path):
             w = i[0][1][0] - i[0][0][0] 
             h = i[0][2][1] - i[0][1][1]
             c.setFont("나눔고딕",h/1.6)
-            c.drawString(x,736-(y-h/2),i[1])
+            c.drawString(x,1275-(y+h/2),i[1])
         c.showPage()
     
     c.save()
